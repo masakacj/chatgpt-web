96a820333d6a59c1adc2be58c22c61ca0011c48f
masakacj <34531511+masakacj@users.noreply.github.com>
2026-09-25 09:33:16 +0800

    Restore minimal unified iOS shell

---
+++ App/ChatGPTWebApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ChatGPTWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(nil)
+        }
+    }
+}
